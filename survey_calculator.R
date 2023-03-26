f3b1a4c5ed8a280242f7dbc07419fa1312cf5bf8
suedamarine <vinsterproshapes@hotmail.com>
2023-03-26 16:13:32 +0200

    first commit

---
+++ survey_calculator.R
@@ -0,0 +1,6 @@
+library(rcompanion)
+library(tidyverse)
+
+## import file - in future create new data.frame from CSV, using vectors to import count & mass into individual columns
+
+temp_cockle <- read.csv("data/temp_cockle.csv")
